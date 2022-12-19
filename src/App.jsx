4ce3ff036675a41b6f9f7955ecb24b4f15a67588
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-12-19 02:33:46 -0500

    Add difficulty

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,39 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const shuffleArray = (unshuffled) => {
+    let shuffled = unshuffled
+      .map(value => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value)
+    return shuffled;
+  }
+
+  const [difficulty, setDifficulty] = useState(4)
+  const [hexArray, setHexArray] = useState([
+    { hexString: 'FF0000', correct: true },
+    { hexString: '00FF00', correct: false },
+    { hexString: '0000FF', correct: false },
+  ]);
+  const [shuffledArray, setShuffledArray] = useState(shuffleArray(hexArray))
+  const [score, setScore] = useState(0);
+
+  //update the shuffle array anytime hex array is updated
+  useEffect(() => {
+    setShuffledArray(shuffleArray(hexArray));
+  }, [hexArray])
+
+  useEffect(() => {
+    if (score >= 10) {
+      setDifficulty(5);
+    } else if (score >= 5) {
+      setDifficulty(4);
+    } else if (score >= 0) {
+      setDifficulty(3);
+    } else {
+      setDifficulty(2);
+    };
+  }, [score])
 
   const generateHexes = (num) => {
     const hexes = [];
@@ -25,34 +58,10 @@ function App() {
     return string;
   }
 
-  const shuffleArray = (unshuffled) => {
-    let shuffled = unshuffled
-      .map(value => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value)
-    return shuffled;
-  }
-
-  // const [difficulty, setDifficulty] = useState(3)
-  const [hexArray, setHexArray] = useState(generateHexes(3));
-  const [shuffledArray, setShuffledArray] = useState(shuffleArray(hexArray))
-  const [answer, setAnswer] = useState(hexArray[0].hexString);
-  const [score, setScore] = useState(0);
-
-  useEffect(() => {
-    setAnswer(hexArray[0].hexString);
-    setShuffledArray(shuffleArray(hexArray));
-
-  }, [hexArray])
-
-
   const rightGuess = () => {
     console.log('right')
-    setHexArray((generateHexes(3)));
-    setAnswer(hexArray[0].hexString);
+    setHexArray((generateHexes(difficulty)));
     setScore((prevScore) => prevScore + 1);
-    // setShuffledArray(shuffleArray(hexArray))
-    // shuffleArray(hexArray)
   }
 
   const wrongGuess = () => {
@@ -62,7 +71,7 @@ function App() {
 
   const BOXSTYLE = {
     position: 'relative',
-    backgroundColor: `#${answer}`,
+    backgroundColor: `#${hexArray[0].hexString}`,
     width: '8rem',
     height: '6rem',
     margin: 'auto',
@@ -78,7 +87,7 @@ function App() {
         </div>
 
         {shuffledArray.map((hex) =>
-          <button key={hex.hexString} onClick={hex.correct ? (e) => rightGuess(e) : wrongGuess}>{hex.hexString}</button>
+          <button key={hex.hexString} onClick={hex.correct ? rightGuess : wrongGuess}>{hex.hexString}</button>
         )}
         <p>{score}</p>
       </div>
