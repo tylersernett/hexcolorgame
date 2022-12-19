4246209837f1d006bb687f9d52f69dd29c5c88a4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-12-19 00:57:23 -0500

    Replace string array with object array

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
-
 function App() {
-  const generateHex = () => {
+  const generateHexes = (num) => {
     const hexChar = "0123456789ABCDEF";
-    let hexString = "";
-    for (let i = 0; i < 6; i++) {
-      hexString += hexChar.charAt(Math.floor(Math.random() * 16));
+
+    const hexes = [];
+    for (let n = 0; n < num; n++) {
+      let hexObj = {
+        hexString: "",
+        correct: n === 0 ? true : false,
+      };
+      for (let i = 0; i < 6; i++) {
+        hexObj.hexString += hexChar.charAt(Math.floor(Math.random() * 16));
+      }
+      hexes.push(hexObj);
+
     }
-
-    return hexString
+    console.log(hexes)
+    return hexes
   }
-  
-  const [hexArray, setHexArray] = useState([generateHex(), generateHex(), generateHex()]);
 
+  const rightGuess = () => {
+    setHexArray(generateHexes(3));
+    setScore((prevScore) => prevScore + 1);
+  }
+
+  const [hexArray, setHexArray] = useState(generateHexes(3));
+  const [score, setScore] = useState(0);
 
   const BOXSTYLE = {
     position: 'relative',
-    backgroundColor: `#${hexArray[0]}`,
+    backgroundColor: `#${hexArray[0].hexString}`,
     width: '8rem',
     height: '6rem',
     margin: 'auto',
     textAlign: 'center',
     marginTop: '2rem',
     padding: '2rem'
-
   }
 
   return (
@@ -36,8 +47,9 @@ function App() {
         </div>
 
         {hexArray.map((hex) =>
-          <button key={hex} onClick={() => setHexArray([generateHex(), generateHex(), generateHex()])}>{hex}</button>
+          <button key={hex.hexString} onClick={hex.correct? rightGuess : ''}>{hex.hexString}</button>
         )}
+        <p>{score}</p>
       </div>
     </>
   );
