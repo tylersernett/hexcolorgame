620847b144d3a13bbb54d6d8213893e29ce28bb9
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-16 23:19:10 -0400

    Add color 'spreading' function to replace old random function

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   const [streak, setStreak] = useState(0);
   const [theme, setTheme] = useState('dark');
 
-  //update the shuffle array anytime hex array is updated
+  //update the shuffle array/scramble displayed options anytime hex array is updated
   useEffect(() => {
     setShuffledArray(shuffleArray(hexArray));
   }, [hexArray])
@@ -38,17 +38,18 @@ function App() {
     if (score < 0) {
       setDifficulty(2);
     } else {
-      setDifficulty(Math.floor((score + 1) / 5) + 3)
+      setDifficulty(Math.floor((score + 1) / 5) + 3); //the number of color options each round
     }
   }, [score])
 
   //create an array of objects: {hexString, correct bool, touched bool}
   const generateHexes = (num) => {
     const hexes = [];
+    const hexStrings = generateColors(num);
     for (let n = 0; n < num; n++) {
       let hexObj = {
-        hexString: randomHex(),
-        correct: n === 0 ? true : false, //0th is always correct answer
+        hexString: hexStrings[n],
+        correct: n === 0 ? true : false, //0th is always correct answer before it's shuffled
         touched: false,
       };
       hexes.push(hexObj);
@@ -57,39 +58,57 @@ function App() {
     return hexes
   }
 
-  //TODO: use algo to define ranges instead of random generation
-  //dificulty: # of splits  [3 answers, 3 splits]
-  //split each R,G,B value ...
-  //range:   0123456789ABCDE   or   123456789ABCDEF
-  //split=3    01234 56789 ABCDE    or   12345 6789A BCDEF
-  //buffer=2    123   678   BCD     spread=3
-  //buffer=3   012    678    CDE    spread=3
-  //trim=2      123   678   BCD     
-  //trim=4       2     7     C    
-  
-  //split=3    01234 56789 ABCDE 
-  //Randselect1    4
-  //apply buffer: process 56789 if val > buffer+Randselect1 (buffer=2, so 56 excluded)
-  //Randselect2          9
-  //apply buffer: process ABCDE if val > buffer+Randselect2 (buffer=2, so AB excluded)
-  //Randselect3              C
-  //shuffle so order is randomized
-  //Answer1 R = Randselect2
-  //Answer2 R = Randselect3
-  //Answer3 R = Randselect1
-  //repeat for G, B values
-
-
-  //1 split it
-  //2 trim edges
-  //3 select random from what remains
-  const randomHex = () => {
-    const hexChar = "0123456789ABCDEF";
-    let string = ""
-    for (let i = 0; i < 6; i++) {
-      string += hexChar.charAt(Math.floor(Math.random() * 16));
+  function generateColors(numColors) {
+    const interval = 360 / numColors;
+    let hue = Math.random() * 360;
+    const colors = [];
+    const startingSat = Math.random();
+    const startingVal = Math.random();
+    for (let i = 0; i < numColors; i++) {
+      hue = (hue + interval) % 360;
+      let sat = (startingSat + (i / numColors)) % 1
+      let val = (startingVal + (i / numColors)) % 1
+      console.log('hue', hue)
+      const rgb = hsvToRgb(hue / 360, sat, val);
+      const hexColor = rgbToHex(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+      colors.push(hexColor);
     }
-    return string;
+    return colors;
+  }
+
+  //https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+  function hsvToRgb(h, s, v) {
+    let r, g, b;
+    const i = Math.floor(h * 6);
+    const f = h * 6 - i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+      case 0:
+        r = v; g = t; b = p; break;
+      case 1:
+        r = q; g = v; b = p; break;
+      case 2:
+        r = p; g = v; b = t; break;
+      case 3:
+        r = p; g = q; b = v; break;
+      case 4:
+        r = t; g = p; b = v; break;
+      case 5:
+        r = v; g = p; b = q; break;
+      default:
+    }
+    return [r, g, b];
+  }
+
+  function rgbToHex(r, g, b) {
+    return componentToHex(Math.round(r)) + componentToHex(Math.round(g)) + componentToHex(Math.round(b));
+  }
+
+  function componentToHex(c) {
+    const hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
   }
 
   //when answer is submitted, momentarily hide (remove the fade class), then add the fade class to restart the animation
